c70eb4d1d9c61a16a7e66d81f6710657ae2a9d5a
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-07-05 11:46:13 -0500

    nav bolds/changes color of section when it is clicked or scrolled to with useState/useEffect in Nav component

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,19 +1,78 @@
+import React, { useState, useEffect } from 'react';
 import './Nav.css'
 
 const Nav = () => {
+  const [activeSection, setActiveSection] = useState('home');
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const sections = document.querySelectorAll('section');
+      const scrollPosition = window.scrollY;
+
+      sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+
+        if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
+          setActiveSection(section.id);
+        }
+      });
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const handleNavItemClick = (sectionId) => {
+    setActiveSection(sectionId);
+
+    const section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' });
+  };
+
+
   return (
     <nav className="navbar">
       <a href="/" className="name">
         <h1 >Elizabeth Olsavsky</h1>
       </a>
       <ul className="nav-list">
-          <li><a href="#about" className="nav-link">About</a></li>
-          <li><a href="#projects" className="nav-link">Projects</a></li>
-          <li><a href="#resume" className="nav-link">Resume</a></li>
-          <li><a href="#contact" className="nav-link">Contact</a></li>
+        <li>
+          <a href="#about"
+            className={`nav-link ${activeSection === 'about' ? 'active' : ''}`}
+            onClick={() => handleNavItemClick('about')}>
+              About
+          </a>
+        </li>
+        
+        <li>
+          <a href="#projects"
+            className={`nav-link ${activeSection === 'projects' ? 'active' : ''}`}
+            onClick={() => handleNavItemClick('projects')}>
+              Projects
+          </a>
+        </li>
+        
+        <li>
+          <a href="#resume"
+            className={`nav-link ${activeSection === 'resume' ? 'active' : ''}`}
+            onClick={() => handleNavItemClick('resume')}>
+              Resume
+          </a>
+        </li>
+
+        <li>
+          <a href="#contact"
+            className={`nav-link ${activeSection === 'contact' ? 'active' : ''}`}
+            onClick={() => handleNavItemClick('contact')}>
+              Contact
+          </a>
+        </li>
       </ul>
     </nav>
-    
   )
 }
 
