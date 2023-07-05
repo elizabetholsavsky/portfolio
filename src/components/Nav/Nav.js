0a841d3eed43c6de7e8a5b78a2cf3fa93353d410
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-07-05 13:30:19 -0500

    fix scroll on nav, now sets nav-link as active on scroll with useRef/IntersectionObserver, scrolls smoothly from one section to another on click

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,79 +1,118 @@
-import React, { useState, useEffect } from 'react';
-import './Nav.css'
+import React, { useState, useEffect, useRef } from 'react';
+import './Nav.css';
 
 const Nav = () => {
   const [activeSection, setActiveSection] = useState('home');
+  const sectionRefs = useRef({});
+
+  const handleNavItemClick = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
 
   useEffect(() => {
-    const handleScroll = () => {
-      const sections = document.querySelectorAll('section');
-      const scrollPosition = window.scrollY;
-
-      sections.forEach((section) => {
-        const sectionTop = section.offsetTop;
-        const sectionHeight = section.clientHeight;
-
-        if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
-          setActiveSection(section.id);
+    const handleScroll = (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setActiveSection(entry.target.id);
         }
       });
     };
 
-    window.addEventListener('scroll', handleScroll);
+    const observer = new IntersectionObserver(handleScroll, {
+      rootMargin: '-50% 0% -50% 0%', 
+    });
+
+    const sections = ['home', 'about', 'projects', 'resume', 'contact'];
+    sections.forEach((sectionId) => {
+      const section = document.getElementById(sectionId);
+      if (section) {
+        observer.observe(section);
+        sectionRefs.current[sectionId] = section;
+      }
+    });
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      sections.forEach((sectionId) => {
+        observer.unobserve(sectionRefs.current[sectionId]);
+      });
     };
   }, []);
 
-  const handleNavItemClick = (sectionId) => {
-    setActiveSection(sectionId);
+  useEffect(() => {
+    const handleSmoothScrollEnd = () => {
+      const sections = ['home', 'about', 'projects', 'resume', 'contact'];
+      const visibleSections = sections.filter((sectionId) => {
+        const section = sectionRefs.current[sectionId];
+        if (section) {
+          const rect = section.getBoundingClientRect();
+          return rect.top >= 0 && rect.bottom <= window.innerHeight;
+        }
+        return false;
+      });
 
-    const section = document.getElementById(sectionId);
-    section.scrollIntoView({ behavior: 'smooth' });
-  };
+      if (visibleSections.length > 0) {
+        setActiveSection(visibleSections[0]);
+      }
+    };
 
+    window.addEventListener('scroll', handleSmoothScrollEnd);
+
+    return () => {
+      window.removeEventListener('scroll', handleSmoothScrollEnd);
+    };
+  }, []);
 
   return (
     <nav className="navbar">
       <a href="/" className="name">
-        <h1 >Elizabeth Olsavsky</h1>
+        <h1>Elizabeth Olsavsky</h1>
       </a>
       <ul className="nav-list">
         <li>
-          <a href="#about"
+          <a
+            href="#about"
             className={`nav-link ${activeSection === 'about' ? 'active' : ''}`}
             onClick={() => handleNavItemClick('about')}>
-              About
-          </a>
-        </li>
-        
-        <li>
-          <a href="#projects"
-            className={`nav-link ${activeSection === 'projects' ? 'active' : ''}`}
-            onClick={() => handleNavItemClick('projects')}>
-              Projects
-          </a>
-        </li>
-        
-        <li>
-          <a href="#resume"
-            className={`nav-link ${activeSection === 'resume' ? 'active' : ''}`}
-            onClick={() => handleNavItemClick('resume')}>
-              Resume
+            About
           </a>
         </li>
 
         <li>
-          <a href="#contact"
-            className={`nav-link ${activeSection === 'contact' ? 'active' : ''}`}
+          <a
+            href="#projects"
+            className={`nav-link ${
+              activeSection === 'projects' ? 'active' : ''
+            }`}
+            onClick={() => handleNavItemClick('projects')}>
+            Projects
+          </a>
+        </li>
+
+        <li>
+          <a
+            href="#resume"
+            className={`nav-link ${activeSection === 'resume' ? 'active' : ''}`}
+            onClick={() => handleNavItemClick('resume')}>
+            Resume
+          </a>
+        </li>
+
+        <li>
+          <a
+            href="#contact"
+            className={`nav-link ${
+              activeSection === 'contact' ? 'active' : ''
+            }`}
             onClick={() => handleNavItemClick('contact')}>
-              Contact
+            Contact
           </a>
         </li>
       </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
