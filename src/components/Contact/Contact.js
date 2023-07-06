0186de2244a1c20fef5916593368b286b95caba4
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-07-05 22:12:18 -0500

    add root color to css, import google font to main css, link main css to other css pages, add 'back to top' link at footer, footer still needs to be positioned

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,12 +1,22 @@
+import './Contact.css'
+
 const Contact = () => {
-  return (
+    return (
     <>
-      <section id="contact">
-        <h2> Contact </h2>
-      </section>
-      <footer></footer>
+        <section id="contact">
+            <div className='contact-content'>
+                <h2> Contact </h2>
+            </div>
+
+            <footer> 
+                <a href="/">
+                    <h3>Back to Top</h3>
+                </a> 
+                <h3 className='copyright'>Elizabeth Olsavsky 2023</h3>
+            </footer>
+        </section>
     </>
     
-  )
+    )
 }
 export default Contact
