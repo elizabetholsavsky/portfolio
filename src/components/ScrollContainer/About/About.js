134c84c15b4dc1aad368c20255813c435a1183e4
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-07-06 13:30:29 -0500

    add basic styles to scroll container css sheet to keep elements, margins, and styles consistent across pages (also make editing and adjustments later easier)

--- src/components/ScrollContainer/About/About.js
+++ src/components/ScrollContainer/About/About.js
@@ -4,10 +4,11 @@ import ProfilePicture from '../images/Elizabeth.jpg'
 const About = () => {
     return (
         <section id="about">
-            <div className="container">
+            <div className="content-container">
                 <div className="about-text">
                     <h2>About</h2>
                     <h3>Web Developer</h3>
+                    <h4>Austin, TX</h4>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique eos fugiat reiciendis aperiam, voluptatum eveniet quisquam quibusdam explicabo corporis, vero, non omnis consequatur numquam cum porro? Hic repellat officia iste!</p>
                 </div>
 
