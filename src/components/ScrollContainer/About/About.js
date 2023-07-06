753d5c4e4a5889605cc3f37cd372562f688270ee
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-07-06 13:02:39 -0500

    added lorem text to format about area with flex, created img folder in src folder for optimization, adjusted flex properties to make about page responsive, adjusted flex properties of footer that were affected (change to flex colum so footer items are listed)

--- src/components/ScrollContainer/About/About.js
+++ src/components/ScrollContainer/About/About.js
@@ -1,8 +1,21 @@
+import './About.css'
+import ProfilePicture from '../images/Elizabeth.jpg'
+
 const About = () => {
-  return (
-    <section id="about">
-      <h2> About </h2>
+    return (
+        <section id="about">
+            <div className="container">
+                <div className="about-text">
+                    <h2>About</h2>
+                    <h3>Web Developer</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique eos fugiat reiciendis aperiam, voluptatum eveniet quisquam quibusdam explicabo corporis, vero, non omnis consequatur numquam cum porro? Hic repellat officia iste!</p>
+                </div>
+
+                <div className='profile-picture'>
+                    <img src={ProfilePicture} alt="Elizabeth Olsavsky" className='elizabeth'/>
+                </div>
+            </div>
     </section>
-  )
+)
 }
 export default About
