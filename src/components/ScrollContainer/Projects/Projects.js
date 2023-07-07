652c063bc1a63e416dfb40e9874405b5ef5cb1b9
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-07-07 10:57:11 -0500

    update other components with content containers and content text with h2 heading

--- src/components/ScrollContainer/Projects/Projects.js
+++ src/components/ScrollContainer/Projects/Projects.js
@@ -1,8 +1,12 @@
 const Projects = () => {
-  return (
-    <section id="projects">
-      <h2> Projects </h2>
-    </section>
-  )
+    return (
+        <section id="projects">
+            <div className="content-container">
+                <div className="content-text">
+                <h2> Projects </h2>
+                </div>
+            </div>
+        </section>
+        )
 }
 export default Projects
