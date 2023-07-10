39e97086448d4d685789c7fcb8e938ac2e787ecd
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-07-10 18:56:56 -0500

    format about jsx

--- src/components/ScrollContainer/About/About.js
+++ src/components/ScrollContainer/About/About.js
@@ -11,34 +11,33 @@ const About = () => {
     return (
         <section aria-label="about" id="about">
             <div className="about-wrapper">
-            
-            <div className="about-content-container">
-                <div className="content-text">
-                    <h2>About</h2>
-                    <h3>Elizabeth Olsavsky</h3>
-                    <h4>Web Developer</h4>
-                    <h5><SvgMapMarker/>Austin, TX</h5>
-                </div>
+                <div className="about-content-container">
+                    <div className="content-text">
+                        <h2>About</h2>
+                        <h3>Elizabeth Olsavsky</h3>
+                        <h4>Web Developer</h4>
+                        <h5><SvgMapMarker/>Austin, TX</h5>
+                    </div>
 
-                <div className="socials-container">
-                        <a href="https://www.linkedin.com/in/elizabetholsavsky/" target="_blank"><SvgLinkedin title="LinkedIn"/></a>
-                        <a href="https://github.com/elizabetholsavsky" target="_blank"><SvgGithub title="Github"/></a>
-                        <a href="https://twitter.com/elizavsky" target="_blank"><SvgTwitter title="Twitter"/></a>
-                        <a href="https://elizabetholsavsky.hashnode.dev/" target="_blank"><SvgHashnode title="Hashnode"/></a>
-                </div>
+                    <div className="socials-container">
+                            <a href="https://www.linkedin.com/in/elizabetholsavsky/" target="_blank"><SvgLinkedin title="LinkedIn"/></a>
+                            <a href="https://github.com/elizabetholsavsky" target="_blank"><SvgGithub title="Github"/></a>
+                            <a href="https://twitter.com/elizavsky" target="_blank"><SvgTwitter title="Twitter"/></a>
+                            <a href="https://elizabetholsavsky.hashnode.dev/" target="_blank"><SvgHashnode title="Hashnode"/></a>
+                    </div>
 
-                <div className="about-card">
-                    <div className="about-text-container">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique eos fugiat reiciendis aperiam, voluptatum eveniet quisquam quibusdam explicabo corporis, vero, non omnis consequatur numquam cum porro? Hic repellat officia iste!</p>
-                        <br/>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique eos fugiat reiciendis aperiam, voluptatum eveniet quisquam quibusdam explicabo corporis, vero, non omnis consequatur numquam cum porro? Hic repellat officia iste!</p>
+                    <div className="about-card">
+                        <div className="about-text-container">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique eos fugiat reiciendis aperiam, voluptatum eveniet quisquam quibusdam explicabo corporis, vero, non omnis consequatur numquam cum porro? Hic repellat officia iste!</p>
+                            <br/>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique eos fugiat reiciendis aperiam, voluptatum eveniet quisquam quibusdam explicabo corporis, vero, non omnis consequatur numquam cum porro? Hic repellat officia iste!</p>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div className='profile-picture-container'>
-                        <img src={ProfilePicture} alt="Elizabeth Olsavsky" className='elizabeth'/>
-            </div>
+                <div className='profile-picture-container'>
+                    <img src={ProfilePicture} alt="Elizabeth Olsavsky" className='elizabeth'/>
+                </div>
             </div>
     </section>
 )
