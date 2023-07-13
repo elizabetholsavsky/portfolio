b90cd9a1b0d4eb74381dc8f3b1bf1f04f6616c0e
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-07-13 16:53:59 -0500

    revise svg files to import css style sheet, remove defined fill

--- src/components/ScrollContainer/SVG/Moon.js
+++ src/components/ScrollContainer/SVG/Moon.js
@@ -1,15 +1,16 @@
-import * as React from "react";
+import React from 'react';
+import "./SVG.css"
+
 const SvgMoon = (props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="1em"
-    height="1em"
+    width="40px"
+    height="40px"
     fill="none"
     viewBox="0 0 24 24"
     {...props}
   >
     <path
-      stroke="#000"
       strokeLinecap="round"
       strokeLinejoin="round"
       strokeWidth={2}
