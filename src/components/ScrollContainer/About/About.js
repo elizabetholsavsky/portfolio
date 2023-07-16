71f0a568e3fd4ffa15703c98b5669e787956b99c
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-07-16 16:41:59 -0500

    add about text

--- src/components/ScrollContainer/About/About.js
+++ src/components/ScrollContainer/About/About.js
@@ -37,7 +37,9 @@ const About = () => {
 
                     <div className="about-card">
                         <div className="about-text-container">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique eos fugiat reiciendis aperiam, voluptatum eveniet quisquam quibusdam explicabo corporis, vero, non omnis consequatur numquam cum porro? Hic repellat officia iste!</p>
+                            <p>
+                                I design and build intuitive and accessible web applications. As a former educator, my passion and commitment to learning remains steadfast. This unique background allows me to approach coding challenges with an empathetic and user-centric perspective, ensuring that my solutions not only functional but provide a seamless and empowering experience for users of all backgrounds.
+                            </p>
                         </div>
                     </div>
                 </div>
