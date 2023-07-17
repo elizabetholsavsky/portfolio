e64846dfbe28755a6a708e4a4e2ad4d432ee03cc
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-07-17 13:24:45 -0500

    add dark mode theme to body using root var in css

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import Header from './components/Header/Header.js'
 import ScrollContainer from './components/ScrollContainer/ScrollContainer.js';
 
 function App() {
-  return (
-    <>
-    <Header/>
-    <ScrollContainer/>
-    </> 
-  ) 
+    return (
+        <>
+            <Header/>
+            <ScrollContainer/>
+        </> 
+    ) 
 };
 
 export default App;
