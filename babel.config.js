e3bc27cae5b478e82ec9ba62e90496100bc2679d
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-07-22 09:53:10 -0500

    target modern browsers

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    presets: [
+        [
+            "@babel/preset-env",
+            {
+                targets: {
+                    browsers: "> 1%, not dead"
+                }
+            }
+        ]
+    ]
+};
